db723b7cb4302c2c8212a7fee539d99778a34b40
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-02-27 20:07:29 +0000

    Fixed serdes issue

--- scalaEx/src/main/scala/kstreams/serdes/CustomSerdes.scala
+++ scalaEx/src/main/scala/kstreams/serdes/CustomSerdes.scala
@@ -6,11 +6,11 @@ import org.apache.kafka.streams.scala.serialization.Serdes
 import io.circe.syntax._
 
 trait GenericSerde {
-  def create[T: Encoder: Decoder] : Serde[T]
+  def create[T >: Null : Encoder: Decoder] : Serde[T]
 }
 
 class CustomSerdes extends GenericSerde {
-  def create[T : Encoder : Decoder]: Serde[T] = {
+  def create[T >: Null : Encoder : Decoder]: Serde[T] = {
     Serdes.fromFn((input: T) =>  input.asJson.noSpaces.getBytes(), (input: Array[Byte]) => input.asJson.as[T] match {
         case Left(_) => None
         case Right(value) => Option(value)
--- scalaEx/src/main/scala/kstreams/topology/BookTopology.scala
+++ scalaEx/src/main/scala/kstreams/topology/BookTopology.scala
@@ -26,7 +26,14 @@ object BookTopology {
     val twiBranch: KStream[Array[Byte], Books] = branch.get("twi").get
     val otherBranch: Option[KStream[Array[Byte], Books]] = branch.get("other")
 
-    val transformedBooks: KStream[Array[Byte], Books] = otherBranch.get.mapValues(_.author.toUpperCase)
+    val transformedBooks: KStream[Array[Byte], Books] = otherBranch.get.mapValues { value =>
+      value match {
+        case it  @ Italian(name, _, _) => it.copy(name = s"$name  mapping")
+        case eng @ English(name, _, _) => eng.copy(name = s"$name is eng")
+        case gh  @ Ghanaian(name, _, _) => gh.copy(name = s"$name no y3 f3")
+        case ger @ German(name, _, _) => ger.copy(name = s"$name Das is good")
+      }
+    }
 
     val mergedStream: KStream[Array[Byte], Books] = twiBranch.merge(transformedBooks)
 
