25a44204a268f3048c9f36d5d8aadbc2789340ec
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-03-04 22:06:31 +0000

    Fixed issue of join error

--- scalaEx/src/main/scala/kstreams/topology/UserPostMain.scala
+++ scalaEx/src/main/scala/kstreams/topology/UserPostMain.scala
@@ -2,7 +2,7 @@ package kstreams.topology
 
 import kstreams.serdes.GenericSerde
 import model.{Post, Users, UsersPost}
-import org.apache.kafka.streams.kstream.Joined
+import org.apache.kafka.streams.scala.kstream.Joined
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{Consumed, KStream, KTable}
 import org.apache.kafka.streams.scala.serialization.Serdes
@@ -11,14 +11,12 @@ class UserPostMain(customSerdes: GenericSerde) {
 
   val builder = new StreamsBuilder()
 
-  val users: KTable[String, Users] = builder.table[String, Users]("users")(Consumed.`with`[String, Users](
-    Serdes.stringSerde, customSerdes.create[Users]))
+  val users: KTable[Int, Users] = builder.table[Int, Users]("users")(Consumed.`with`[Int, Users](
+    Serdes.intSerde, customSerdes.create[Users]))
 
   val post: KStream[Int, Post] = builder.stream[String, Post]("post")(Consumed.`with`[String, Post](
       Serdes.stringSerde, customSerdes.create[Post]))
     .selectKey((_, post) => post.userId)
-
-  //  def join[VT, VR](table: KTable[K, VT])(joiner: (V, VT) => VR)(implicit joined: Joined[K, V, VT]): KStream[K, VR]
 
   val joinedParams: Joined[Int, Post, Users] = Joined
     .`with`[Int, Post, Users](
