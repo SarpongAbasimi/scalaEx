8ad2d0167529f6918aa47c0c3cbd5c98b6bd4c5a
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-03-30 23:21:11 +0000

    Printing count

--- scalaEx/src/main/scala/kstreams/topology/WindowTopology.scala
+++ scalaEx/src/main/scala/kstreams/topology/WindowTopology.scala
@@ -2,7 +2,8 @@ package kstreams.topology
 
 import kstreams.serdes.CustomSerdes
 import model.{BodyTemperature, Pulse}
-import org.apache.kafka.streams.kstream.{TimeWindows, Windowed}
+import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.{Printed, TimeWindows, Windowed}
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{Consumed, Grouped, KStream, KTable, Materialized}
 import org.apache.kafka.streams.scala.serialization.Serdes
@@ -10,18 +11,28 @@ import org.apache.kafka.streams.scala.serialization.Serdes
 import java.time.Duration
 
 class WindowTopology(customSerdes: CustomSerdes) {
-  val streamBuilder: StreamsBuilder = new StreamsBuilder()
 
-  val pulseEvents: KStream[String, Pulse] = streamBuilder
-    .stream("pulse-event")(Consumed.`with`[String, Pulse](Serdes.stringSerde, customSerdes.create[Pulse]))
+  def create: Topology = {
+    val streamBuilder: StreamsBuilder = new StreamsBuilder()
 
-  val bodyTemperature: KStream[String, BodyTemperature] = streamBuilder
-    .stream("body-temp-event")(Consumed.`with`[String,BodyTemperature](Serdes.stringSerde, customSerdes.create[BodyTemperature]))
+    val pulseEvents: KStream[String, Pulse] = streamBuilder
+      .stream("pulse-event")(Consumed.`with`[String, Pulse](Serdes.stringSerde, customSerdes.create[Pulse]))
 
-  val pulseCount: KTable[Windowed[String], Long] = pulseEvents.groupByKey(Grouped.`with`(Serdes.stringSerde, customSerdes.create[Pulse]))
-    .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(60)))
-    .count()(Materialized.as("pulse-count")(Serdes.stringSerde, Serdes.longSerde))
+    val bodyTemperature: KStream[String, BodyTemperature] = streamBuilder
+      .stream("body-temp-event")(Consumed.`with`[String, BodyTemperature](Serdes.stringSerde, customSerdes.create[BodyTemperature]))
 
-  
+    val pulseCount: KTable[Windowed[String], Long] = pulseEvents.groupByKey(Grouped.`with`(Serdes.stringSerde, customSerdes.create[Pulse]))
+      .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(60)))
+      .count()(Materialized.as("pulse-count")(Serdes.stringSerde, Serdes.longSerde))
+
+    pulseCount
+      .toStream
+      .print(
+        Printed.toSysOut[Windowed[String], Long]
+          .withLabel("pulse-count")
+      )
+
+    streamBuilder.build()
+  }
 
 }
